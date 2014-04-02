de906f9ddd33ad0069df06908ef59d0dcf039dbe
Aneesh Neelam <neelam.aneesh@gmail.com>
2014-04-02 20:46:50 +0530

    Version 2 Test

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('Second Test')
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
